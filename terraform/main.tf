446e754bc44830407601482425bb032dabf30c34
cielo2498 <130421192+cielo2498@users.noreply.github.com>
2023-06-08 00:20:13 -0400

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,80 @@
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
+  key_name      = "cielo"
+}
+
+resource "aws_key_pair" "cielo" {
+  key_name   = "cielo"
+  public_key = file("${path.module}/cielo.pub")
+}
+
+
+resource "aws_security_group" "cielo" {
+  name        = "clo835_assignment1"
+  description = "Allow inbound traffic"
+  vpc_id      = "vpc-0bf19b010418a18c1"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "MYSQL/Aurora from everywhere"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "1"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "2"
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "3"
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "prefix" {
+  description = "Prefix for the resources"
+  type        = string
+  default     = "default"
+}
